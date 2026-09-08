9e65d6f03430570a98f186b7b7e7b09627b06ce4
Nurjigit1891 <117098636+Nurjigit1891@users.noreply.github.com>
2026-09-08 18:31:34 +0600

    fixed scss import casing 3

--- src/pages/moviesschedulepage/Moviesschedulepage.jsx
+++ src/pages/moviesschedulepage/Moviesschedulepage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './MoviesSchedulepage.scss';
+import './Moviesschedulepage.scss';
 import Header from '../../components/header/Header';
 
 // Замени на реальные данные с бэкенда
